220a1ae6b46e773bbe54015437586a7ccb01fb2a
mlkiung <meredith.kiung@gmail.com>
2017-08-01 16:33:20 -0400

    server

---
+++ start.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {resolve} = require('path');
+
+const app = express();
+
+app.use(require('volleyball'));
+
+module.exports = app
+  .use(express.static(resolve(__dirname, '..', 'build')))
+  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'build', 'index.html')))
+  .listen(process.env.PORT || 8080);
